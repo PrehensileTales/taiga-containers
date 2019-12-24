3bd0c3ab175695c7cdb325236c317f4cc5b86951
Hein-Pieter van Braam-Stewart <hp@tmm.cx>
2019-12-24 12:44:11 +0100

    Add setting for celery threads

--- start-celery.sh
+++ start-celery.sh
@@ -4,4 +4,9 @@ set -e
 
 export C_FORCE_ROOT=1
 
-taiga/bin/celery -A taiga worker --concurrency 4 -l INFO --uid apache
+if [ -z "${TAIGA_CELERY_PROCESSES}" ]; then
+  echo "TAIGA_CELERY_PROCESSES not set, defaulting to 4!"
+  TAIGA_CELERY_PROCESSES=4
+fi
+
+taiga/bin/celery -A taiga worker --concurrency ${TAIGA_CELERY_PROCESSES} -l INFO --uid apache
